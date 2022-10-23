2dc37d1ad62bef138631a93d4bd5dcc5158a0f41
PC <rayan.alhourani@yahoo.com>
2022-10-23 21:42:51 +0300

    add delete user

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const authController = require("../Controllers/authController")
 
+
 const router =express.Router()
 
 router.get('/signup',authController.signup_get)
@@ -8,6 +9,7 @@ router.post('/signup',authController.signup_post)
 router.get('/login',authController.login_get)
 router.post('/login',authController.login_post)
 router.get('/logout',authController.logout_get)
+router.delete('/deleteUser',authController.deleteUser)
 
 
 
