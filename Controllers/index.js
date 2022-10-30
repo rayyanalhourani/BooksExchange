f41fb849db55f1fb823f098fc097f03bbdd9cea1
PC <rayan.alhourani@yahoo.com>
2022-10-30 20:38:24 +0200

    routes files in 1 file

---
+++ Controllers/index.js
@@ -0,0 +1,29 @@
+const express = require("express")
+const authController = require("../Controllers/authController")
+const bookController = require("../Controllers/bookController")
+
+const {authorization} = require("../middleware/authMiddleware")
+
+
+
+const router =express.Router()
+//authRouter
+router.get('/signup',authController.signup_get)
+router.post('/signup',authController.signup_post)
+router.get('/login',authController.login_get)
+router.post('/login',authController.login_post)
+router.get('/logout',authController.logout_get)
+router.delete('/deleteUser',authController.deleteUser)
+
+
+//bookRouter
+router.post("/books",authorization ,bookController.books_post)
+router.get("/books",authorization,bookController.books_get)
+router.delete("/books",authorization,bookController.books_delete)
+
+
+
+module.exports = router;
+
+
+
