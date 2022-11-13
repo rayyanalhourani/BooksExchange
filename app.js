c5996302d8a63fa6228b8e8981e8f3045b80af14
rayyanalhourani <rayyan.alhourani02@gmail.com>
2022-11-13 22:32:05 +0300

    solve body is empty

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 const express = require("express");
 const app = express();
 const Routes = require("./Controllers/index")
-
 const cookiesParser = require('cookie-parser')
+const bodyParser = require('body-Parser')
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(express.json()); 
 app.use(cookiesParser())
 app.use(Routes)
+app.set("view engine", "ejs");
+app.use(express.static('public'))
+app.use(express.urlencoded());
 
 
+
+
+
+  
 app.get("/", (req, res) => {
     res.status(200).send("home page");
 });
