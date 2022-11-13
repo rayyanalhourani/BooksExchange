35a833efe4fa0a3778641a73544c776e36fa2340
rayyanalhourani <rayyan.alhourani02@gmail.com>
2022-11-13 22:54:04 +0300

    some edit

--- app.js
+++ app.js
@@ -5,13 +5,11 @@ const cookiesParser = require('cookie-parser')
 const bodyParser = require('body-Parser')
 
 app.use(bodyParser.urlencoded({extended: true}));
-
 app.use(express.json()); 
 app.use(cookiesParser())
 app.use(Routes)
 app.set("view engine", "ejs");
 app.use(express.static('public'))
-app.use(express.urlencoded());
 
 
 
