9e5af0adf30e138751e551124800085d1673ea6d
rayyanalhourani <rayyan.alhourani02@gmail.com>
2022-11-29 20:53:19 +0300

    solve the stupid wat :p

--- Controllers/bookController.js
+++ Controllers/bookController.js
@@ -1,20 +1,30 @@
-const jwtDecode = require("jwt-decode")
-let { getconnection } = require("../Model/database")
+const jwtDecode = require("jwt-decode");
+let { getconnection } = require("../Model/database");
+
+module.exports.home_get = (req, res) => {
+    const sql = `select * from book`;
+
+    let data = "hi";
+
+    getconnection().query(sql, (err, result) => {
+        if (err) {
+            res.status(404).send(err);
+        } else {
+            res.render("../Views/home.ejs", { result });
+        }
+    });
+};
 
 module.exports.AddBook_get = (req, res) => {
     res.render("../Views/AddBook.ejs");
-}
-
-module.exports.home_get= (req, res) => {
-    res.render("../Views/home.ejs")
 };
 
 module.exports.AddBook_post = (req, res) => {
-    let bookid = req.body.bookid
-    let name = req.body.name
-    let collage = req.body.collage
-    let token = req.cookies.jwt
-    let ownerId = jwtDecode(token).id
+    let bookid = req.body.bookid;
+    let name = req.body.name;
+    let collage = req.body.collage;
+    let token = req.cookies.jwt;
+    let ownerId = jwtDecode(token).id;
     const sql = `INSERT INTO book (id,name ,collage, book_owner_id ) VALUES ("${bookid}","${name}","${collage}","${ownerId}")`;
 
     getconnection().query(sql, (err, result) => {
@@ -24,11 +34,9 @@ module.exports.AddBook_post = (req, res) => {
             res.status(201).send("book added successfully");
         }
     });
-
-}
+};
 
 module.exports.books_get = (req, res) => {
-
     const sql = `select * from book`;
 
     getconnection().query(sql, (err, result) => {
@@ -38,90 +46,66 @@ module.exports.books_get = (req, res) => {
             res.status(201).send(result);
         }
     });
-}
+};
 
 module.exports.books_delete = (req, res) => {
-    let token = req.cookies.jwt
-    let userid = jwtDecode(token).id
-    let bookid = req.body.bookid
+    let token = req.cookies.jwt;
+    let userid = jwtDecode(token).id;
+    let bookid = req.body.bookid;
 
     let owner_id;
     let userRole;
 
-    function get_id() {
-        let sql = `Select book_owner_id from book where id = ${bookid}`
-
-        try {
-            getconnection().query(sql, (err, result) => {
-                if (err) {
-                    res.status(404).send(err);
-                } else {
-                    owner_id = result[0].book_owner_id;
-                }
-            });
-        }
-        catch {
-            res.status(404).send("you cant delete this book");
-
-        }
-    }
-
-    function get_role() {
-        sql = `Select Role from user where id = ${userid}`
+    let sql = `Select book_owner_id from book where id = ${bookid}`;
+    try {
         getconnection().query(sql, (err, result) => {
             if (err) {
                 res.status(404).send(err);
             } else {
-                userRole = result[0].Role;
+                owner_id = result[0].book_owner_id;
             }
         });
+    } catch {
+        res.status(404).send("you cant delete this book");
     }
 
-
-
-    function delete_book() {
-
-        try {
-            
-            if (userRole === "admin" || owner_id == userid) {
-                sql = `delete from book where id = ${bookid}`;
-                getconnection().query(sql, (err, result) => {
-                    if (err) {
-                        res.status(404).send(err);
-                    } else {
-                        res.status(201).send("book deleted successfully");
-                    }
-                });
-            }
-
-            else {
-                res.send("you dont have permission")
-            }
+    sql = `Select Role from user where id = ${userid}`;
+    getconnection().query(sql, (err, result) => {
+        if (err) {
+            res.status(404).send(err);
+        } else {
+            userRole = result[0].Role;
         }
-        catch {
-            res.status(404).send("you can't delete the book");
+    });
 
-        }
+    try {
+        sql = `delete from book where id = ${bookid}`;
+        getconnection().query(sql, (err, result) => {
+            if (err) {
+                res.status(404).send(err);
+            } else {
+                if (userRole === "admin" || owner_id == userid) {
+                    res.status(201).send("book deleted successfully");
+                } else {
+                    res.send("you dont have permission");
+                }
+            }
+        });
+    } catch {
+        res.status(404).send("you can't delete the book");
     }
+};
 
-    get_id()
-    get_role()
-    setTimeout(delete_book, 5000)
+module.exports.myBooks_get = (req, res) => {
+    let token = req.cookies.jwt;
+    let userid = jwtDecode(token).id;
 
-}
-
-module.exports.myBooks_get= (req,res)=>{
-    let token = req.cookies.jwt
-    let userid = jwtDecode(token).id
-    
     let sql = `select name from book where book_owner_id = ${userid}`;
-                getconnection().query(sql, (err, result) => {
-                    if (err) {
-                        res.status(404).send(err);
-                    } else {
-                        res.status(201).send(result);
-                    }
-                });
-
-
-}
+    getconnection().query(sql, (err, result) => {
+        if (err) {
+            res.status(404).send(err);
+        } else {
+            res.status(201).send(result);
+        }
+    });
+};
